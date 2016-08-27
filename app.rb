b2edf8a6e2beea6ac8e28a46688a20ba7fba3507
Bruno Abrantes <bruno@brunoabrantes.com>
2016-08-27 13:42:50 +0200

    Starting the consumption process will be delegated to a binary later

--- app.rb
+++ app.rb
@@ -5,5 +5,3 @@ Dir[APP_ROOT / "config" / "initializers" / "**" / "*.rb"].each { |f| require f }
 require "app/models/source"
 require "app/concepts/deployment"
 require "app/consumers/build_finishes"
-
-Consumers::BuildFinishes.new.consume!
