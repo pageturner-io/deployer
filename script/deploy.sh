7e4707401d8971afe745b4720ecd1181ee95b9aa
Bruno Abrantes <bruno@brunoabrantes.com>
2016-08-30 23:20:19 +0200

    Containerizes application with a God monitored process

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
 
-export REPO=pageturner/builder
+export REPO=pageturner/deployer
 export TAG=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo "latest"; else echo $TRAVIS_BRANCH ; fi`
 
 docker build -f Dockerfile -t $REPO:$TRAVIS_COMMIT .
