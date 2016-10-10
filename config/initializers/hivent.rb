c90940aa31dfd3cdaaff89b47f14f191cc48c01d
Bruno Abrantes <bruno@brunoabrantes.com>
2016-10-10 16:33:51 +0200

    Makes Hivent partition_count configurable

--- config/initializers/hivent.rb
+++ config/initializers/hivent.rb
@@ -1,6 +1,6 @@
 Hivent.configure do |config|
   config.backend         = :redis
   config.endpoint        = ENV["HIVENT_URL"]
-  config.partition_count = 4
+  config.partition_count = ENV["HIVENT_PARTITION_COUNT"].to_i
   config.client_id       = "deployer"
 end
